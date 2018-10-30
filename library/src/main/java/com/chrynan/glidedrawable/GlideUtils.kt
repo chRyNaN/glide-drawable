e61bcf7abdaf18e871a99613d010ec3483a18560
Chris <ckeenan@starry.com>
2018-10-30 10:33:32 -0400

    Added RequestManager.loadFunction function

--- library/src/main/java/com/chrynan/glidedrawable/GlideUtils.kt
+++ library/src/main/java/com/chrynan/glidedrawable/GlideUtils.kt
@@ -4,6 +4,20 @@ package com.chrynan.glidedrawable
 
 import android.graphics.drawable.Drawable
 import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.Key
+import com.bumptech.glide.signature.ObjectKey
+
+const val DRAWABLE_FUNCTION_KEY_PREFIX = "drawable:function"
+
+fun RequestManager.loadFunction(function: () -> Drawable): RequestBuilder<Drawable> =
+        load(object : DrawableFunction {
+
+            override val uniqueKey: Key = ObjectKey("$DRAWABLE_FUNCTION_KEY_PREFIX:${function.hashCode()}")
+
+            override val function: () -> Drawable
+                get() = function
+        })
 
 fun RequestBuilder<Drawable>.listener(listener: DrawableListener) = listener(DrawableRequestListener(listener))
 
